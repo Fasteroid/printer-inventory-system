0fd3d3817c90c98afc0a146855d1f4a0c21f4b39
Fasteroid <fasteroid@hotmail.com>
2021-03-12 18:28:05 -0500

    added a BUNCH of stuff
    + list system almost works now
    + sorting behavior finished
    + code is starting to spaghettify

--- global.js
+++ global.js
@@ -1,1 +1,2 @@
-const PRINTER_ATTRIBUTES = ["Bar Code","Category","Location","Serial Number","Manufacturer","Division","Department","Campus"]
+/** Global declaration of all attributes printers have; can be modified to suit changing demands */
+const PRINTER_ATTRIBUTES = ["Serial Number","Name"]
--- list.js
+++ list.js
@@ -1,17 +1,83 @@
-const HEADERS = document.getElementById("table-header-box")
+class ListElement {
 
-// fill in the list headers after the page loads
-for (const key in PRINTER_ATTRIBUTES) {
-    let headerTitle = PRINTER_ATTRIBUTES[key];
-    let header = document.createElement("th"); // table header
-    let clickable = document.createElement("button"); // table header
-    clickable.className = "tableHeader";
-    clickable.id        = headerTitle;
-    clickable.innerText = headerTitle;
-    clickable.onclick   = function(){
-        // TODO: sorting functionality
-        sortHeadersBy(headerTitle); 
+    constructor(htmlRef){
+        for (const attribute of PRINTER_ATTRIBUTES) {
+            this[attribute] = "missing";
+        }
+        this.htmlRef = htmlRef;
     }
-    header.appendChild(clickable);
-    HEADERS.prepend(header); // prepend since we want to keep the action header at the end
+
+    remove(){
+        this.htmlRef.remove();
+    }
+
 }
+
+
+// ---------------- <GENERATE LIST HEADER BUTTONS> ---------------- //
+const ListHeaderHTML = document.getElementById("list-headers")
+
+for (const attribute of PRINTER_ATTRIBUTES) {
+
+    let data = document.createElement("th");      // table header for <table>
+    data.innerText = attribute;
+
+    //button.onclick   = function(){
+    //    // TODO: sorting functionality
+    //    sortHeadersBy(attribute); 
+    //}
+
+    ListHeaderHTML.prepend(data); // prepend since we want to keep the action header at the end
+
+}
+
+// ---------------- <GENERATE LIST INPUT UTILITY> ---------------- //
+const ListInputHTML = document.getElementById("list-inputs")
+const ListInputs    = { }
+let   FirstInput;   // will be the first input so we can focus it after adding something
+for (const attribute of PRINTER_ATTRIBUTES) {
+
+    let data = document.createElement("td");     // table data for <table>
+    let input  = document.createElement("input");  // button to place inside it
+    input.className = "tableInput"
+
+    ListInputs[attribute] = input;  // store these for easy access later
+    data.appendChild(input);      // insert our <input> into our <td>
+    ListInputHTML.prepend(data);  // prepend since we want to keep the add button at the end
+
+    FirstInput = input; // since we're prepending elements this will actually be the correct thing
+
+}
+
+const ListHTML = document.getElementById("list")
+const List     = [ ]
+
+/** 
+ * Adds an element to the internal list of elements from the input fields.
+ * Clears the input fields.
+ */
+function AddListElement(){
+
+    let row = document.createElement("tr") // create new row for list
+    let element = new ListElement(row);
+
+    for (const attribute of PRINTER_ATTRIBUTES) {
+
+        let value = ListInputs[attribute].value;
+        element[attribute] = value;
+
+        let data = document.createElement("td"); // table data for <table>
+        data.className = "entry";                // format this text like the table entry boxes
+
+        data.innerText = value;   // Add the HTML for this entry
+        row.prepend(data);
+
+        ListInputs[attribute].value = ""; // clear out the inputs as we read them
+    
+    }  
+    List.push(element);
+    ListHTML.append(row);
+    FirstInput.focus();  // focus the first input so that the user can rapid-fire add stuff with tab :)
+
+}
+
