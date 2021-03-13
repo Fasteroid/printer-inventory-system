258b9c4baaa4ed2b3272707cc4cb275179ae5a9b
Fasteroid <fasteroid@hotmail.com>
2021-03-13 02:21:24 -0500

    refactored things-old way wasn't gonna work

--- global.js
+++ global.js
@@ -1,2 +1,2 @@
 /** Global declaration of all attributes printers have; can be modified to suit changing demands */
-const PRINTER_ATTRIBUTES = ["Serial Number","Name"]
+const PRINTER_ATTRIBUTES = ["Campus","Department","Division","Manufacturer","Serial Number","Location","Category","Bar Code"]
--- list.js
+++ list.js
@@ -1,83 +1,98 @@
-class ListElement {
 
-    constructor(htmlRef){
+
+const ListHeaderHTML = document.getElementById("list-headers");
+const ListInputHTML  = document.getElementById("list-inputs");
+const ListHTML       = document.getElementById("list");
+const ListInputs     = { };
+let   Printers       = [ ];
+let   FirstInput;    // will be the first input so we can focus it after adding something
+
+
+/** Holds serializable data pertaining to printers */
+class ListData {
+    /** 
+     * Creates ListData and pushes it onto `Printers`
+     * Set any property on it from `PRINTER_ATTRIBUTES` 
+    */
+    constructor(){
         for (const attribute of PRINTER_ATTRIBUTES) {
-            this[attribute] = "missing";
+            this[attribute] = "<unset>";
         }
-        this.htmlRef = htmlRef;
+        Printers.push(this);
     }
 
+    /** Removes ListData from ListElements. */
     remove(){
-        this.htmlRef.remove();
+        Printers = Printers.filter( item => item!=this );
     }
-
 }
 
+/** Used to create the HTML for entries of the printer list */
+class ListEntry {
+    constructor( object = new ListData() ){ // use a default ListData if one isn't provided
 
-// ---------------- <GENERATE LIST HEADER BUTTONS> ---------------- //
-const ListHeaderHTML = document.getElementById("list-headers")
+        let self = this; // avoid confusing behavior runtime-evaluation of 'this'
 
-for (const attribute of PRINTER_ATTRIBUTES) {
+        self.HTML = document.createElement("tr"); // create table row
+            for (const attribute of PRINTER_ATTRIBUTES) {
+                let value = object[attribute];
+                let data = document.createElement("td"); // create table data
+                    data.className = "entry";
+                    data.innerText = value;
+                self.HTML.prepend(data); // end table data
+            }  
+        
+            let data = document.createElement("td"); // create table data
+                let button = document.createElement("button"); // create button
+                    button.innerText = "Remove";
+                    button.onclick = function(){
+                        self.HTML.remove();
+                        object.remove();
+                    }
+            data.append(button); // end table data
+        self.HTML.append(data); // end table row
 
-    let data = document.createElement("th");      // table header for <table>
-    data.innerText = attribute;
-
-    //button.onclick   = function(){
-    //    // TODO: sorting functionality
-    //    sortHeadersBy(attribute); 
-    //}
-
-    ListHeaderHTML.prepend(data); // prepend since we want to keep the action header at the end
-
+    }
 }
-
-// ---------------- <GENERATE LIST INPUT UTILITY> ---------------- //
-const ListInputHTML = document.getElementById("list-inputs")
-const ListInputs    = { }
-let   FirstInput;   // will be the first input so we can focus it after adding something
-for (const attribute of PRINTER_ATTRIBUTES) {
-
-    let data = document.createElement("td");     // table data for <table>
-    let input  = document.createElement("input");  // button to place inside it
-    input.className = "tableInput"
-
-    ListInputs[attribute] = input;  // store these for easy access later
-    data.appendChild(input);      // insert our <input> into our <td>
-    ListInputHTML.prepend(data);  // prepend since we want to keep the add button at the end
-
-    FirstInput = input; // since we're prepending elements this will actually be the correct thing
-
-}
-
-const ListHTML = document.getElementById("list")
-const List     = [ ]
 
 /** 
  * Adds an element to the internal list of elements from the input fields.
  * Clears the input fields.
  */
 function AddListElement(){
-
-    let row = document.createElement("tr") // create new row for list
-    let element = new ListElement(row);
-
+    let data = new ListData();
     for (const attribute of PRINTER_ATTRIBUTES) {
-
-        let value = ListInputs[attribute].value;
-        element[attribute] = value;
-
-        let data = document.createElement("td"); // table data for <table>
-        data.className = "entry";                // format this text like the table entry boxes
-
-        data.innerText = value;   // Add the HTML for this entry
-        row.prepend(data);
-
-        ListInputs[attribute].value = ""; // clear out the inputs as we read them
-    
+        let testValue = ListInputs[attribute].value
+        if( testValue != "" ){ // avoid setting nonexistence
+            data[attribute] = testValue;
+        }
+        ListInputs[attribute].value = ""; // clear out values as we read them
     }  
-    List.push(element);
-    ListHTML.append(row);
-    FirstInput.focus();  // focus the first input so that the user can rapid-fire add stuff with tab :)
+
+    let entry = new ListEntry( data )
+    ListHTML.prepend( entry.HTML ); 
+    FirstInput.focus();  // focus the first input so that the user can rapid-fire add stuff with tab
+}
+
+// ---------------- <GENERATE LIST HEADER BUTTONS> ---------------- //
+for (const attribute of PRINTER_ATTRIBUTES) {
+
+    let data = document.createElement("th"); // create table header
+        data.innerText = attribute;
+    ListHeaderHTML.prepend(data); // end table header - prepend to keep 'Action' header at the end
 
 }
 
+// ---------------- <GENERATE LIST INPUT UTILITY> ---------------- //
+for (const attribute of PRINTER_ATTRIBUTES) {
+
+    let data = document.createElement("td"); // create table data
+        let input  = document.createElement("input");  // create input
+            input.className = "tableInput"
+        data.appendChild(input); // end input
+    ListInputHTML.prepend(data); // end table data
+
+    ListInputs[attribute] = input;  // store these for easy access later
+    FirstInput = input; // since we're prepending elements this will actually be the correct thing in the end
+    
+}
