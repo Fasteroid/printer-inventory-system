0f7ea2ad5a13b31511523f24c313d3e595513a9d
Fasteroid <fasteroid@hotmail.com>
2021-03-16 21:58:20 -0400

    headers and standardization

--- verification.js
+++ verification.js
@@ -1,10 +1,20 @@
+/*
+    verification.js
+
+    Login checker base by Javier.
+    TODO: security
+
+    First Edit:  Javier
+    Authors:  Javier
+*/
+
 function checkPswd() {
     var dummyPassword = "password";
     var dummyUsername = "admin";
     var password = document.getElementById("pswd").value;
     var username = document.getElementById("usrnm").value;
     if (password == dummyPassword && username == dummyUsername) {
-         window.location="index.html";
+        window.location = "index.html"; // point the user to the printer list for now
     }
     else{
         alert("Passwords do not match.");
