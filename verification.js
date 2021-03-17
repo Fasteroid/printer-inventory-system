4ac63a2266b78d40106796c90968ec3cade84638
SRGSEAL6 <73493854+SRGSEAL6@users.noreply.github.com>
2021-03-16 21:24:29 -0400

    login html with a simple verification with js
    username = admin
    password = password

---
+++ verification.js
@@ -0,0 +1,12 @@
+function checkPswd() {
+    var dummyPassword = "password";
+    var dummyUsername = "admin";
+    var password = document.getElementById("pswd").value;
+    var username = document.getElementById("usrnm").value;
+    if (password == dummyPassword && username == dummyUsername) {
+         window.location="success.html";
+    }
+    else{
+        alert("Passwords do not match.");
+    }
+}
