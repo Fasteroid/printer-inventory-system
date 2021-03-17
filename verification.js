aa653ae740ef52720b366d76a6938e4a04d2bf2e
SRGSEAL6 <73493854+SRGSEAL6@users.noreply.github.com>
2021-03-16 21:36:49 -0400

    Update verification.js
    changed it so it sends you to index.html

--- verification.js
+++ verification.js
@@ -4,7 +4,7 @@ function checkPswd() {
     var password = document.getElementById("pswd").value;
     var username = document.getElementById("usrnm").value;
     if (password == dummyPassword && username == dummyUsername) {
-         window.location="success.html";
+         window.location="index.html";
     }
     else{
         alert("Passwords do not match.");
