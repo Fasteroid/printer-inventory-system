896be228986993d544c1f67dcbf91d3eae60c836
Fasteroid <fasteroid@hotmail.com>
2021-03-24 13:23:02 -0400

    move user.js and add header

--- user.js
+++ user.js
@@ -1,6 +1,14 @@
+/*
+    user.js
+
+    User class for holding user data.  Shared.
+
+    First Edit:  Javier
+    Authors:  Javier
+*/
+
 class User {
     constructor(email, password, permission){
-        
         this.email = email;
         this.password = password;
         this.permission = permission;
