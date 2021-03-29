40fbf9685417970ff2ecfeb059cc1911b49832e8
SRGSEAL6 <73493854+SRGSEAL6@users.noreply.github.com>
2021-03-29 13:38:22 -0400

    Made this to resemble the printer funtions
    hi

--- Login/verification.js
+++ Login/verification.js
@@ -7,21 +7,14 @@
     First Edit:  Javier
     Authors:  Javier
 */
- const fs = require('fs');
 
  function checkPswd() {
          var password = document.getElementById("pswd").value;
          var username = document.getElementById("usrnm").value;
-         fs.readFile('userInfo.json','utf8',(err, file) => {
-             if (err) throw err;
-             else{
-                 const data =  JSON.parse(file);
-                 if (password == data.password && username == data.email) {
-                     window.location = "index.html"; // point the user to the printer list for now
-                 }
-                  else{
-                     alert("Passwords do not match.");
-                 }
-             }
-         });   
+         if (password == data.password && username == data.email) {
+            window.location = "index.html"; // point the user to the printer list for now
+        }
+         else{
+            alert("Passwords do not match.");
+        } 
  }
--- user.js
+++ user.js
@@ -8,11 +8,25 @@
 */
 
 class User {
-    constructor(email, password, permission){
-        if(email){ // do nothing if noarg
-            this.email = email;
-            this.password = password;
-            this.permission = permission;
+
+    // constructor(email, password, permission){
+    //     if(email){ // do nothing if noarg
+    //         this.email = email;
+    //         this.password = password;
+    //         this.permission = permission;
+    //     }
+    // }
+    static attributes = {
+        email:      "Email",
+        password:    "Password",
+        permission:     "Permission",
+    }
+    /** Constructs a new printer object. Don't modify anything in this class from outside. 
+     * @param obj data to be turned into a printer
+    */
+    constructor(obj = {}){
+        for( let property in User.attributes ){
+            this[property] = obj[property];
         }
     }
 }
