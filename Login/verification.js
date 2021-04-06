967821d0ea460d1d3cf106c980788a42ddcac02a
SRGSEAL6 <73493854+SRGSEAL6@users.noreply.github.com>
2021-04-05 21:27:23 -0400

    Some
    cant get it to work after line 41

--- Login/verification.js
+++ Login/verification.js
@@ -6,14 +6,51 @@
 
     First Edit:  Javier
     Authors:  Javier
-*//** Long polling loop. Periodically refreshed by the server and supplies updates on data changes */
- function checkPswd() {
-         var password = document.getElementById("pswd").value;
-         var username = document.getElementById("usrnm").value;
-         if (password == Database.token && username == Database.email) {
-            window.location = "index.html"; // point the user to the printer list for now
+*/
+/** Sends a json-based request to the server
+ * @param {JSON} data token, server command, additional data
+ */
+
+ async function ServerCommand(data){
+    return await fetch(
+        "http://68.84.141.134",
+        {
+            headers: {
+                "content-type":"application/json",
+            },
+            body: JSON.stringify(data),
+            method: "POST"
         }
-         else{
-            alert("Passwords do not match.");
-        } 
- }
+    );
+}
+async function init(){
+
+    // ask the server for the printers list, store the response in data
+    let data = await ServerCommand({
+        command: "getUsers"
+    })
+
+    // convert the response to a javascript object
+    let json = await data.json()
+
+    // log it to make sure it looks good
+    //console.log(json)
+    return json
+}
+async function checkPswd(){
+    let users = await init()
+    console.log(users);
+    var password = document.getElementById("pswd").value;
+    var username = document.getElementById("usrnm").value;
+    
+    // for(var i=0; i<users.length; i++) {
+    //    if(users[i].email == username) {
+    //        // __FOUND is set to the index of the element
+    //        window.location = "index.html";
+    //        break;
+    //    }
+    //    else{
+    //        console.log("Cant find users")
+    //   }
+    // }
+}
